b8ddb281de66f6665764f41542bac61e54bf215a
Dwaraka Somarouthu <69767647+idwaraka@users.noreply.github.com>
2020-09-29 18:34:34 +0530

    Update webaction.php

--- webaction.php
+++ webaction.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-echo "you are a noob please dont visit this site next , your device may get hacked....";
+$txt = "you are a noob please dont visit this site next , your device may get hacked...";
+echo "I love $txt!";
 ?>
-
-</body> </html>
